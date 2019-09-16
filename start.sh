c9db13ea79d7e48e46de2ce7f9409fdc61f0b188
danielisgr8 <schubertmdaniel@gmail.com>
2019-09-17 00:53:38 +0100

    Now using pm2 to handle crashes in start.sh.

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 cd ./spotify-song-range/server
-npm start -- -r "https://danielschubert.dev/spotify-song-range/" -p 1001 &
+pm2 start --name spotify-song-range npm -- start -- -r "https://danielschubert.dev/spotify-song-range/" -p 1001
 cd ../../Spotify-Tracker
-npm start -- -r "https://danielschubert.dev/spotify-tracker/callback/" -p 1002 &
+pm2 start --name spotify-tracker npm -- start -- -r "https://danielschubert.dev/spotify-tracker/callback/" -p 1002
 cd ../Playlist-Art
-npm start -- -r "https://danielschubert.dev/playlist-art/art/" -p 1003 &
+pm2 start --name playlist-art npm -- start -- -r "https://danielschubert.dev/playlist-art/art/" -p 1003
 cd ..
-npm start
+pm2 start --name proxy npm -- start
