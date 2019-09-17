ccb6ea1c31f8808defcb15b29c7e9454e835b7b0
danielisgr8 <schubertmdaniel@gmail.com>
2019-09-17 20:43:47 +0100

    Improved landing page.

--- public/main.js
+++ public/main.js
@@ -1,12 +1,29 @@
 const projects = Array.from(document.querySelector(".project-list").children);
 const descriptions = Array.from(document.querySelector(".descriptions").children);
+const gifSrcs = descriptions.map((desc) => desc.querySelector("img").src);
 
 projects.forEach((el, i) => {
-	el.onmouseover = (event) => {
-		descriptions[i].style.display = "initial";
-	};
-	el.onmouseout = (event) => {
-		descriptions[i].style.display = "none";
+	el.onclick = (event) => {
+		event.preventDefault();
+
+		descriptions.forEach((desc, j) => {
+			if(j !== i) {
+				desc.style.display = "none";
+				desc.querySelector("img").src = "#";
+			}
+		});
+		const oldDisplay = descriptions[i].style.display;
+		const opening = oldDisplay === "none" || oldDisplay === "";
+		let display, gifSrc;
+		if(opening) {
+			display = "block";
+			gifSrc = gifSrcs[i];
+		} else {
+			display = "none";
+			gifSrc = "#";
+		}
+		descriptions[i].style.display = display;
+		descriptions[i].querySelector("img").src = gifSrc;
 	};
 });
 
