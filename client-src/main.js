27f6d1a43b6e05616388ea836cd87ee828c041b2
danielisgr8 <schubertmdaniel@gmail.com>
2019-09-25 05:51:26 +0100

    The body of the homepage now rendered through React.

--- client-src/main.js
+++ client-src/main.js
@@ -1,29 +1,62 @@
-const projects = Array.from(document.querySelector(".project-list").children);
-const descriptions = Array.from(document.querySelector(".descriptions").children);
-const gifSrcs = descriptions.map((desc) => desc.querySelector("img").src);
+const spotifyDemos = [
+	{
+		"title": "Spotify Song Range",
+		"demoLink": "./spotify-song-range/",
+		"gifLink": "https://danielschubert.dev/gifs/spotify-song-range.gif",
+	},
+	{
+		"title": "Playlist Art",
+		"demoLink": "./playlist-art/",
+		"gifLink": "https://danielschubert.dev/gifs/playlist-art.gif"
+	},
+	{
+		"title": "Spotify Tracker",
+		"demoLink": "./spotify-tracker/",
+		"gifLink": "https://danielschubert.dev/gifs/spotify-tracker.gif",
+	},
+];
 
-projects.forEach((el, i) => {
-	el.onclick = (event) => {
-		event.preventDefault();
+const ProjectLink = ({title, onClick}) => (
+	<p className="demo-link" onClick={() => onClick()}>{title}</p>
+)
 
-		descriptions.forEach((desc, j) => {
-			if(j !== i) {
-				desc.style.display = "none";
-				desc.querySelector("img").src = "#";
-			}
-		});
-		const oldDisplay = descriptions[i].style.display;
-		const opening = oldDisplay === "none" || oldDisplay === "";
-		let display, gifSrc;
-		if(opening) {
-			display = "block";
-			gifSrc = gifSrcs[i];
-		} else {
-			display = "none";
-			gifSrc = "#";
-		}
-		descriptions[i].style.display = display;
-		descriptions[i].querySelector("img").src = gifSrc;
-	};
-});
+const Description = ({title, gifLink, demoLink}) => (
+	<div>
+		<h2>{title}</h2>
+		<img src={gifLink} />
+		<a href={demoLink}><h2>Demo</h2></a>
+	</div>
+);
+
+const App = () => {
+	/*
+	 If project === -1, no description will be shown.
+	 Otherwise, the description represented by spotifyDemos[project] will be shown.
+	*/
+	const [project, setProject] = React.useState(-1);
+
+	const projects = spotifyDemos.map((demo, i) => (
+		<li key={demo.title}><ProjectLink title={demo.title} onClick={() => setProject(project === i ? -1 : i)} /></li>
+	));
+
+	return (
+		<>
+		<div className="projects">
+			<h2>Project Demos</h2>
+			<div className="notes">
+                                <p>Note: If any errors or long requests are encountered, navigating back to the given project's login page should fix the issue.</p>
+                                <p>Note: All of the below demos requires a Spotify account (ideally with songs and playlists in their library).</p>
+                        </div>
+			<ul className="project-list">
+				{projects}
+                        </ul>
+                </div>
+                <div className="description">
+			{project >= 0 ? <Description {...spotifyDemos[project]} /> : null}
+		</div>
+		</>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector(".app"));
 
