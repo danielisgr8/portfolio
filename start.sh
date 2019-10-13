aab308cbf27454f8c6a7fb7fe8de47c4b2251d5a
danielisgr8 <schubertmdaniel@gmail.com>
2019-10-13 20:10:03 +0100

    pm2 process logs now have timestamps

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 cd ./spotify-song-range/server
-pm2 start --name spotify-song-range npm -- start -- -r "https://danielschubert.dev/spotify-song-range/" -p 1001
+pm2 start --name spotify-song-range --time npm -- start -- -r "https://danielschubert.dev/spotify-song-range/" -p 1001
 cd ../../Spotify-Tracker
-pm2 start --name spotify-tracker npm -- start -- -r "https://danielschubert.dev/spotify-tracker/callback/" -p 1002
+pm2 start --name spotify-tracker --time npm -- start -- -r "https://danielschubert.dev/spotify-tracker/callback/" -p 1002
 cd ../Playlist-Art
-pm2 start --name playlist-art npm -- start -- -r "https://danielschubert.dev/playlist-art/art/" -p 1003
+pm2 start --name playlist-art --time npm -- start -- -r "https://danielschubert.dev/playlist-art/art/" -p 1003
 cd ..
-pm2 start --name proxy npm -- start
+pm2 start --name proxy --time npm -- start
